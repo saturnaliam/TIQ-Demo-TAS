a0fbdc14803e8d0c4a3ceeccf626dd8a386b7a34
saturnaliam <llcsaturnaliam@gmail.com>
2023-05-16 21:06:14 -0500

    added "ui"

--- main.cpp
+++ main.cpp
@@ -11,25 +11,27 @@
 #include "include/tm.hpp"
 #include "include/gmb.hpp"
 
-void leftClick() {  
+INPUT sendClick = {0};
+INPUT sendUp = {0};
+inline void leftClick() {  
     INPUT Input = {0};
     for(int i = 0; i <= 1; ++i) {
-        Input.type = INPUT_MOUSE;
-        Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-        ::SendInput(1, &Input, sizeof(INPUT));
-        
-        ::ZeroMemory(&Input, sizeof(INPUT));
-        Input.type = INPUT_MOUSE;
-        Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-        ::SendInput(1, &Input, sizeof(INPUT));
+        ::SendInput(1, &sendClick, sizeof(INPUT));
+
+        ::SendInput(1, &sendUp, sizeof(INPUT));
     }
 }
 
 int main() {
-    DWORD PID;
-    DWORD address;
-    int scene, offsetX, offsetY;
+    sendClick.type = INPUT_MOUSE;
+    sendClick.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    sendUp.type = INPUT_MOUSE;
+    sendUp.mi.dwFlags = MOUSEEVENTF_LEFTUP;
 
+    DWORD PID, address;
+    int scene, offsetX, offsetY;
+    char c;
+    
     const HWND windowHandle = FindWindow(nullptr, L"Adobe Flash Player 32");
     GetWindowThreadProcessId(windowHandle, &PID);
 
@@ -44,13 +46,37 @@ int main() {
     RECT rect = {0};
     GetWindowRect(windowHandle, &rect);
 
-    for (TAS_MOVE movement : movements) {
-        offsetX = movement.cursorPosition.x + rect.left + COORD_OFFSET_X;
-        offsetY = movement.cursorPosition.y + rect.top + COORD_OFFSET_Y;
+    userInput:
+        system("cls");
+        printf("Choose [s]afe or [f]ast execution: ");
+        scanf("%c", &c);
+        if (c == 's') goto safeMove;
+        if (c == 'f') goto fastMove;
+        goto userInput;
 
-        while (scene != movement.scene) ReadProcessMemory(processHandle, (LPVOID)address, &scene, sizeof(scene), &bytesRead);
 
-        SetCursorPos(offsetX, offsetY);
-        leftClick();
-    }
+    safeMove:
+        for (TAS_MOVE movement : movements) {
+            offsetX = movement.cursorPosition.x + rect.left + COORD_OFFSET_X;
+            offsetY = movement.cursorPosition.y + rect.top + COORD_OFFSET_Y;
+
+            while (scene != movement.scene) ReadProcessMemory(processHandle, (LPVOID)address, &scene, sizeof(scene), &bytesRead);
+
+            SetCursorPos(offsetX, offsetY);
+            leftClick();
+        }
+
+        goto endProgram;
+
+    fastMove:
+        for (TAS_MOVE movement : movements) {
+            offsetX = movement.cursorPosition.x + rect.left + COORD_OFFSET_X;
+            offsetY = movement.cursorPosition.y + rect.top + COORD_OFFSET_Y;
+
+            SetCursorPos(offsetX, offsetY);
+            leftClick();
+        }
+
+    endProgram:
+        CloseHandle(processHandle);
 }
